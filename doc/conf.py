09085281b817bb2d1888b7c0fdf82a42ea451ebc
Gergely Csatari <gergely.csatari@nokia.com>
2020-07-03 11:58:41 +0300

    Correcging relative links
    This change adds and utilises a sphinx extension to support the
    relative links when transforming from .md to .html
    
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- doc/conf.py
+++ doc/conf.py
@@ -11,9 +11,21 @@ project = u'CNTT-CNTT'
 
 sys.path.append(os.path.abspath("../direct-copy"))
 sys.path.append(os.path.abspath("../readme-to-index"))
+sys.path.append(os.path.abspath("../relative-link-corrector"))
 
 extensions = ['direct-copy', 
-              'readme-to-index']
+              'readme-to-index', 
+              'relative-link-corrector',
+              'sphinx_markdown_tables']
 
 direct_copy_directories = ['/gov/figures', 
-                           '/ref_model/figures']
+                           '/ref_model/figures', 
+                           '/ref_arch/figures', 
+                           '/ref_arch/kubernetes/figures', 
+                           '/ref_arch/openstack/figures', 
+                           '/ref_cert/lfn/figures', 
+                           '/ref_cert/RC2/figures', 
+                           '/ref_impl/cntt-ri/figures', 
+                           '/ref_impl/cntt-ri2/figures',
+                           '/tech/figures', 
+                           '/ven_impl/figures'] 
