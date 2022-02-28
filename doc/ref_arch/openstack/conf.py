17de488728e556bc51bba3bdd40fb0047e391fe0
Cédric Ollivier <cedric.ollivier@orange.com>
2022-02-28 10:29:37 +0100

    Publish doc build files
    At the time of writting, tox will fail because of pending RST issues.
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ doc/ref_arch/openstack/conf.py
@@ -0,0 +1,17 @@
+project = 'Anuket OpenStack based Reference Architecture'
+copyright = '2021, Anuket'
+author = 'Anuket'
+exclude_patterns = [
+    '.tox',
+    'README.rst'
+]
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel'
+]
+html_theme = "sphinx_material"
+intersphinx_mapping = {
+    'cntt': ('https://cntt.readthedocs.io/en/latest/', None)
+}
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 2
