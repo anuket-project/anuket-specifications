a2c7f96e7f339ac195a0709d6e50b1be529877e4
collivier <ollivier.cedric@gmail.com>
2022-06-10 09:12:08 +0200

    Unify Sphinx config (#3055)
    They are several differences in Sphinx config which breaks
    user naviguation.
    
    The proposal is, by default, to use default Material values,
    and to apply any change to all streams per PR.
    
    It also selects Figure in all captions (default is Fig.).
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    
    Co-authored-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- doc/ref_cert/RC1/conf.py
+++ doc/ref_cert/RC1/conf.py
@@ -24,5 +24,4 @@ autosectionlabel_maxdepth = 4
 numfig = True
 numfig_format = {'figure': 'Figure %s', 'table': 'Table %s',
                  'code-block': 'Listing %s', 'section': 'Section %s'}
-
 latex_theme = 'howto'
