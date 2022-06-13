0729cfb3bfd498cd1981c887c1990e1e94a0cb48
Gergely Csatari <gergely.csatari@nokia.com>
2022-06-13 14:29:05 +0100

    [doc]: Adding Anuket theme back
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,7 @@ author = 'Anuket Project of Linux Foundation Networking'
 extensions = ['sphinx.ext.intersphinx',
               'sphinx.ext.autosectionlabel'
              ]
-html_theme = "sphinx_material"
+
 exclude_patterns = [
     '**/.tox',
     'ref_arch',
@@ -30,3 +30,37 @@ intersphinx_mapping = {
 }
 autosectionlabel_prefix_document = True
 autosectionlabel_maxdepth = 4
+
+html_theme = "sphinx_material"
+html_sidebars = {
+    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+}
+
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
+
+html_theme_options = {
+    'repo_name': 'Material for Sphinx',
+    'nav_title': 'Anuket Specifications',
+     # Set the color and the accent color
+    'color_primary': 'white',
+    'color_accent': 'teal',
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 1,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+    'nav_links': [
+        {
+            'href': 'https://docs.opnfv.org/',
+            'internal': False,
+            'title': 'Anuket Project Documentation'
+        }
+    ]
+}
+
+html_logo = '_static/anuket-logo.png'
+html_favicon = '_static/favicon.ico'
