07ce7202842028990355b0b74b6780970d36cfaf
Cédric Ollivier <cedric.ollivier@orange.com>
2022-12-15 09:25:23 +0100

    Add linkcheck exception for ntia.gov (#3287)
    * Add linkcheck exception for ntia.gov
    
    The certificate has just expired.
    It also updates tox.ini to take latest OpenStack stable
    upper-constraint inito account.
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    
    * Fix major RST issues added by #3137
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    
    * Update RI1 and RI2 tox.ini
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit d9fcfa50ca8187ca9f81a37664fb1a15278dd2ad)

--- doc/ref_cert/RC2/conf.py
+++ doc/ref_cert/RC2/conf.py
@@ -11,7 +11,9 @@ extensions = [
 ]
 html_theme = "sphinx_material"
 linkcheck_ignore = [
-    'http://127.0.0.1'
+    'http://127.0.0.1',
+    'https://github.com/cncf/cnf-testsuite/',
+    'https://github.com/opencontainers/'
 ]
 intersphinx_mapping = {
     'ref_arch_kubernetes': ('https://cntt.readthedocs.io/projects/ra2/en/stable-nile/', None)
