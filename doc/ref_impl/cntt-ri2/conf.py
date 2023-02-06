ad464feabcd481a6cf026e7158b5ab7ffa5675e7
Gergely Csatari <gergely.csatari@nokia.com>
2023-02-06 03:44:27 +0200

    [Ri2]: Fixing some of the issues of 3308 (#1)
    * [Ri2]: Fixing some of the issues of 3308
    
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>
    
    * [RI1]: Resetting accidental changes in RI1
    
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>
    
    ---------
    
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- doc/ref_impl/cntt-ri2/conf.py
+++ doc/ref_impl/cntt-ri2/conf.py
@@ -27,3 +27,21 @@ autosectionlabel_maxdepth = 3
 numfig = True
 numfig_format = {'figure': 'Figure %s', 'table': 'Table %s',
                  'code-block': 'Listing %s', 'section': 'Section %s'}
+
+html_static_path = ['_static']
+templates_path = ['_templates']
+html_css_files = [
+    'custom.css',
+]
+
+html_show_sourcelink = False
+html_theme_options = {
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
+
+# Inverse png
+html_logo = '_static/anuket-logo.png'
+html_favicon = '_static/favicon.ico'
