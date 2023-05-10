4746ce16962defbd6f0cedb41f820601576a2b1f
Gergely Csatari <gergely.csatari@nokia.com>
2023-05-10 12:05:29 +0300

    [RI2]: Adding Piccolo theme
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- doc/ref_impl/cntt-ri/conf.py
+++ doc/ref_impl/cntt-ri/conf.py
@@ -10,6 +10,10 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.autosectionlabel'
 ]
+
+html_theme = "piccolo_theme"
+
+
 linkcheck_ignore = [
     'http://127.0.0.1',
     'https://trex-tgn.cisco.com'
@@ -24,3 +28,22 @@ autosectionlabel_maxdepth = 4
 numfig = True
 numfig_format = {'figure': 'Figure %s', 'table': 'Table %s',
                  'code-block': 'Listing %s', 'section': 'Section %s'}
+
+html_static_path = ['_static']
+templates_path = ['_templates']
+
+html_css_files = [
+    'custom.css',
+]
+
+html_show_sourcelink = False
+html_theme_options = {
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
+
+# Inverse png
+html_logo = '_static/anuket-logo.png'
+html_favicon = '_static/favicon.ico'
