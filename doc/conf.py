89ee14aab9eee6fa01b1a6e5e65841430d994b42
Gergely Csatari <gergely.csatari@nokia.com>
2023-10-18 13:30:17 +0300

    Removing intersphiinx references
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- doc/conf.py
+++ doc/conf.py
@@ -22,16 +22,15 @@ linkcheck_ignore = [
 
 # temporarly I just add all intersphinx mappings needed. later these should be replaced with a single intersphinx to the
 # whole document itself.
-intersphinx_mapping = {
-    'cntt': ('https://cntt.readthedocs.io/en/latest/', None),
-    'ref_model': ('https://cntt.readthedocs.io/projects/rm/en/latest/', None),
-    'ref_arch_kubernetes': ('https://cntt.readthedocs.io/projects/ra2/en/latest/', None),
-    'ref_arch_openstack': ('https://cntt.readthedocs.io/projects/ra1/en/latest/', None),
-    'ref_cert_RC2': ('https://cntt.readthedocs.io/projects/rc2/en/latest/', None),
-    'ref_impl_cntt-ri': ('https://cntt.readthedocs.io/projects/ri1/en/latest/', None),
-    'ref_impl_cntt-ri2': ('https://cntt.readthedocs.io/projects/ri2/en/latest/', None)
-
-}
+#intersphinx_mapping = {
+#    'cntt': ('https://cntt.readthedocs.io/en/latest/', None),
+#    'ref_model': ('https://cntt.readthedocs.io/projects/rm/en/latest/', None),
+#    'ref_arch_kubernetes': ('https://cntt.readthedocs.io/projects/ra2/en/latest/', None),
+#    'ref_arch_openstack': ('https://cntt.readthedocs.io/projects/ra1/en/latest/', None),
+#    'ref_cert_RC2': ('https://cntt.readthedocs.io/projects/rc2/en/latest/', None),
+#    'ref_impl_cntt-ri': ('https://cntt.readthedocs.io/projects/ri1/en/latest/', None),
+#    'ref_impl_cntt-ri2': ('https://cntt.readthedocs.io/projects/ri2/en/latest/', None)
+#}
 
 
 autosectionlabel_prefix_document = True
